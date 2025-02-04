2741dda4c6587c8dd01fec708dcb3cff350eeaa1
3lue4rt <bduartenass@gmail.com>
2025-02-04 10:23:42 -0300

    template to exportxls, with interface

---
+++ exportxls.py
@@ -0,0 +1,33 @@
+from openpyxl import Workbook
+import os
+import re
+from csvHandling import csvData
+
+'''
+interface for exportxls
+it helps export data as excel workbooks
+
+    def expAppend(workbook: Workbook, listData: list[csvData]) -> None:
+        given a list of data, appends data to the end of the active sheet in the workbook
+
+    def expCheck(name: str) -> bool:
+        returns True if workbook exists, False otherwise
+
+    def expSave(Workbook: Workbook, name: str) -> bool:
+        Saves the Workbook with the given name returning True, if it can't, return False
+'''
+
+
+def expAppend(workbook: Workbook, listData: list[csvData]) -> None:
+    for data in listData:
+        workbook.active.append(data.exportList())
+
+def expCheck(name: str) -> bool:
+    splitted = name.split(".")
+    return os.path.exists(splitted[0])
+
+def expSave(Workbook: Workbook, name: str) -> bool:
+    WINDOWS_FILENAME_REGEX = re.compile(r'^(?!^(CON|PRN|AUX|NUL|COM[1-9]|LPT[1-9])$)'  # Disallow reserved names
+                                        r'[^<>:"/\\|?*\x00-\x1F]+'  # Ensure no invalid characters
+                                        r'(?:\.xlsx)?$' ) # Allow only .xlsx extension or no extension at all
+    return bool(WINDOWS_FILENAME_REGEX.match(name)) and len(name) <= 255
