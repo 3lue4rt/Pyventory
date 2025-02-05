f2d63f7139ff30ccafb29970fbc14972c6c8c10c
3lue4rt <bduartenass@gmail.com>
2025-02-05 09:21:43 -0300

    OOP remake

--- programa.py
+++ programa.py
@@ -9,10 +9,19 @@ except ImportError:
     raise ImportError("Se requiere el modulo tkinter")
 
 import types
-
-from template import *
+from exportxls import *
 from csvHandling import *
 
+'''
+interface for tkinter pyventory gui: programa
+it uses the csvHandling file for data storing in a csv
+and the exportxls file for exporting data in a xlsx file (excel)
+
+
+
+    
+'''
+'''
 #creates the Tkinter root
 def rootInit() -> Tk:
     root = Tk()
@@ -105,44 +114,79 @@ class App:
             self.insert3()
         self.entry.bind("<Return>", next)
 
-    def insert3(self):
-        self.label.config(text="Ingrese Partida")
-        def next(x):
-            self.result.append(self.entry.get())
-            self.insert4()
-        self.entry.bind("<Return>", next)
-    
-    def insert4(self):
-        self.label.config(text="Ingrese Procesador")
-        def next(x):
-            self.result.append(self.entry.get())
-            self.insert5()
-        self.entry.bind("<Return>", next)
-    
-    def insert5(self):
-        self.label.config(text="Ingrese RAM")
-        def next(x):
-            self.result.append(self.entry.get())
-            self.insert6()
-        self.entry.bind("<Return>", next)
+'''
+#creates a Button given a frame, name and function
+def createButton(frame: Frame, name: str, func: types.FunctionType) -> Button:
+    button = Button(frame, text=name, font=("Arial", 14), command= func)
+    button.pack(fill="both")
+    return button
 
-    def insert6(self):
-        self.label.config(text="Ingrese SSD")
-        def next(x):
-            self.result.append(self.entry.get())
-            self.insert7()
-        self.entry.bind("<Return>", next)
+#Clears a Frame of all their widgets
+def clear(frame: Frame):
+    for widget in frame.winfo_children():
+        widget.pack_forget()
 
-    def insert7(self):
-        self.label.config(text="Ingrese Ubicación")
-        def next(x):
-            self.result.append(self.entry.get())
-            self.insert8()
-        self.entry.bind("<Return>", next)
+#App class for initializing the application
+class App:
+    def __init__(self):
+        #Creates the root
+        self.root = Tk()
+        self.root.title("Pyventory")
+        self.root.geometry("800x450")
 
-    def insert8(self):
-        self.label.config(text="Ingrese Monitor")
-        def next(x):
-            self.result.append(self.entry.get())
-            self.menu(self.frame1)
-        self.entry.bind("<Return>", next)
+        #Creates the Frame for the menu
+        self.mainFrame = Frame(self.root, padx=10, pady=10)
+        self.mainFrame.pack(fill="both")
+
+        #Creates the Main Menu in the respective Frame
+        self.mainMenu = MainMenu(self.mainFrame)
+
+        #Creates the Frame for the terminal
+        self.terminalFrame = Frame(self.root, padx=10, pady=10)
+        self.terminalFrame.pack(fill="both")
+
+        #Creates the Terminal in the respective Frame
+        self.terminal = Terminal(self.terminalFrame)
+
+    #Method for starting the app
+    def run(self) -> None:
+        self.root.mainloop()
+
+#Terminal class, requires a parent Frame to sit on, displays text
+class Terminal:
+    def __init__(self, parentFrame: Frame):
+        #Parent Frame
+        self.parent = parentFrame
+
+        #Initial text
+        self.text = "=> Pyventory, BlueArt 2025"
+
+        #Creates the Label for displaying text
+        self.mainLabel = Label(self.parent, text=self.text, font=("Arial", 16), justify="left",anchor="sw")
+        self.mainLabel.pack(side="bottom", anchor="sw", fill="x", padx=10, pady=10)
+
+    #Updates the text in the Label with the self.text string
+    def update(self):
+        self.mainLabel.config(text = self.text)
+
+    #Adds a line of text to the bottom of the Label
+    # NEEDS TESTING FOR OVERFLOW
+    def addLine(self, text: str):
+        self.text += "\n=>"+text
+        self.update()
+
+    #Deletes all text in Label
+    def deleteText(self):
+        self.text = "=> "
+        self.update()
+
+#Main Menu class, requires a parent Frame to sit on, buttons for other menus
+class MainMenu:
+    def __init__(self, parentFrame: Frame):
+        #Parent
+        self.parent = parentFrame
+        clear(self.parent)
+        self.insertButton = createButton(self.parent, "Ingresar Computador", lambda : None)
+        self.editButton = createButton(self.parent, "Editar Computador", lambda : None)
+        self.deleteButton = createButton(self.parent, "Eliminar Computador", lambda : None)
+        self.searchButton = createButton(self.parent, "Buscar Computador", lambda : None)
