fc5548c8f1ae20dec4b526a89691fad2e1e6f0d5
3lue4rt <bduartenass@gmail.com>
2025-02-13 09:07:16 -0300

    fix length bug + documentation

--- exportxls.py
+++ exportxls.py
@@ -8,7 +8,7 @@ interface for exportxls
 it helps export data as excel workbooks
 
     def expAppend(workbook: Workbook, listData: list[csvData]) -> None:
-        given a list of data, appends data to the end of the active sheet in the workbook
+        
 
     def expCheck(name: str) -> bool:
         returns True if workbook exists, False otherwise
@@ -17,21 +17,23 @@ it helps export data as excel workbooks
         Saves the Workbook with the given name returning True, if it can't, return False
 '''
 
-
+#given a list of data, appends data to the end of the active sheet in the workbook
 def expAppend(workbook: Workbook, listData: list[csvData]) -> None:
     for data in listData:
         workbook.active.append(data.exportList())
 
+#returns True if workbook exists, False otherwise
 def expCheck(name: str) -> bool:
     splitted = name.split(".")
     return os.path.exists(splitted[0])
 
+#Saves the Workbook with the given name returning True, if it can't, return False
 def expSave(workbook: Workbook, name: str) -> bool:
     WINDOWS_FILENAME_REGEX = re.compile(r'^(?!^(CON|PRN|AUX|NUL|COM[1-9]|LPT[1-9])$)'  # Disallow reserved names
                                         r'[^<>:"/\\|?*\x00-\x1F]+'  # Ensure no invalid characters
                                         r'(?:\.xlsx)?$' ) # Allow only .xlsx extension or no extension at all
-    if not bool(WINDOWS_FILENAME_REGEX.match(name)) or len(name) <= 255:
-        return 0
+    if not bool(WINDOWS_FILENAME_REGEX.match(name)) or len(name) >= 255:
+        return False
 
     result = 2 if name.endswith(".xlsx") else 1
 
